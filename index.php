6526b58871b1401c8fec2dd3bbe677f342ca985f
Zawar Ahmad <zawarbutt94@gmail.com>
2020-12-21 21:34:04 +0500

    hello

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once './pages/header.php';
+?>
+<!--Main Body Start-->
+<main>
+<section>
+
+</section>
+</main>
+<?php
+include_once './pages/footer.php';
+?>
---
+++ pages/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>proj</title>
+    <link rel="shortcut icon" icon="type/x-icon" href="./images/Svg.ico" />
+    <!--Reset Css File-->
+    <link rel="stylesheet" href="./styles/reset.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <!--Internal Css File-->
+    <link rel="stylesheet" href="./styles/style.css" />
+</head>
+<body>
+    <!--Header Start-->
+    <header>
+        <!--Navbar Start-->
+        <nav class="navbar navbar-default navbar-fixed-top navbar-custom" >
+        <div class="container-fluid top-det" ><h4>FREE SHIPPING WORLDWIDE ON ORDER OVER $100 </h6></div>
+  <div class="container-fluid">
+  
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" id="brand-logo" href="./index.php">
+        <img alt="Brand" src="./images/LOGOred2.png">
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-center" id="nav-center">
+        <li class=""><a href="#">Home</a></li>
+        <li><a href="#">Catagory</a></li>
+        <li><a href="#">New</a></li>
+        <li><a href="#">About</a></li>
+        
+      </ul>
+      <ul class="nav navbar-nav navbar-right" id="nav-right">
+        <li><a href="#"><span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+        <!--Navbar End-->
+    </header>
+    <!--Header End-->
+    
